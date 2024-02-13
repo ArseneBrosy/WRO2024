dac4040174f24c979147fdeedff0afcd1b3ee42e
AlexLeProgrammer <alex.etienne@divtec.ch>
2024-02-13 16:29:29 +0100

    Default functions.

--- main.py
+++ main.py
@@ -9,12 +9,34 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import time
 
 ev3 = EV3Brick()
+
+# Motors
 leftMotor = Motor(Port.C)
 rightMotor = Motor(Port.B)
+claw = Motor(Port.D)
+lift = Motor(Port.A)
 
-leftMotor.run_angle(200, -30, wait=False)
-rightMotor.run_angle(200, -30)
+# Functions
+
+# Wheels
+def move2Wheels(power, degrees):
+    leftMotor.run(power)
+    rightMotor.run_angle(power, degrees)
+    leftMotor.hold()
+
+# Claw
+def openClaw():
+    claw.run_time(1000, 500)
+
+def closeClaw():
+    claw.run_time(-1000, 500)
+
+
+# Take the blocks
+move2Wheels(200, -30)
+
 rightMotor.run_angle(500, 360)
-rightMotor.run(200)
-leftMotor.run_angle(200, 1100)
-rightMotor.hold()
+
+move2Wheels(200, 1100)
+
+closeClaw()
